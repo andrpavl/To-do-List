0ebebec68917a83cf52101a913682bb69608cd96
andrpavl <117930401+andrpavl@users.noreply.github.com>
2024-07-01 20:54:50 +0200

    env update

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -7,12 +7,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyCeYeV6DFwJ---DT9fLBKMrFIzdzEi4ipc',
-  authDomain: 'todo-app-c78d2.firebaseapp.com',
-  projectId: 'todo-app-c78d2',
-  storageBucket: 'todo-app-c78d2.appspot.com',
-  messagingSenderId: '252897898133',
-  appId: '1:252897898133:web:66665eec74e1ea0f95f59c',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
